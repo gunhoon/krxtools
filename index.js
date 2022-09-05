41de66e64836ce2aa9a7cf65d21f791de0499453
Gunhoon Lee <gunhoon@gmail.com>
2022-09-06 02:07:40 +0900

    Set the browser user agnet

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [
+            '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+        ],
+        headless: true
+    });
+
+    console.log(await browser.version());
+    console.log(await browser.userAgent());
+
     const page = await browser.newPage();
     await page.goto('https://example.com');
     await page.screenshot({path: 'example.png'});
